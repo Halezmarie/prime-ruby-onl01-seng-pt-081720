761bc9c47caa0cb3fdfb4ef1cf84e1f0258867f2
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-30 16:15:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,31 @@
-describe "prime?" do
-  it 'returns true for prime numbers' do
-  expect(prime?(2)).to be(true)
-  expect(prime?(3)).to be(true)
-  expect(prime?(11)).to be(true)
-  expect(prime?(105557)).to be(true)
+def sieve(max)
+  # Set up an array with all the numbers from 0 to the max
+  primes = (0..max).to_a
+
+  # Set both the first and second positions (i.e., 0 and 1) to nil, as they
+  # aren't prime.
+  primes[0] = primes[1] = nil
+
+  # Iterate through primes array
+  counter = 0
+  primes.each do |p|
+    # Skip if nil
+    next unless p
+
+    # Break if we are past the square root of the max value 
+    break if p*p > max
+    counter += 1
+    # Start at the square of the current number, and step through.
+    # Go up to the max value, by multiples of the current number, and replace
+    # that value with nil in the primes array
+    (p*p).step(max,p) { |m| primes[m] = nil }
+  end
+
+  # Finally, return the compacted array.
+  puts "Solved for #{max} in #{counter} steps."
+  primes.compact
+end
+
+def prime?(num)
+  sieve(num).include?(num)
 end
